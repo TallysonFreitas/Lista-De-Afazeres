9940e61314681e3040a5ed37bb935823d667e22f
Tallyson Freitas <tallyson.eu@outlook.com>
2023-10-27 14:14:36 -0300

    Site Responsivo para cell

--- src/containers/Lista/style.ts
+++ src/containers/Lista/style.ts
@@ -14,11 +14,20 @@ export const SRodapeLista = styled.div`
   align-items: center;
   justify-content: space-between;
   background-color: ${(props) => props.theme.Elements};
+  @media (max-width: 648px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas:
+    "text clear"
+    "filter filter"
 `
 
 export const STextFooter = styled.p`
   color: ${(props) => props.theme.gray};
   font-size: 16px;
+  @media (max-width: 648px) {
+    padding: 8px;
+  }
 `
 
 export const SClearButton = styled.button`
@@ -30,12 +39,24 @@ export const SClearButton = styled.button`
   &:hover {
     color: ${(props) => props.theme.text};
   }
+  @media (max-width: 648px) {
+    padding: 8px;
+  }
 `
 export const SContainerFilter = styled.div`
   max-width: 20%;
   width: 100%;
   display: flex;
   justify-content: space-between;
+  @media (max-width: 648px) {
+    max-width: 100%;
+    order: 2;
+    margin: 0 auto;
+    grid-area: filter;
+    justify-content: center;
+    gap: 32px;
+    margin-top: 24px;
+  }
 `
 
 export const SFilterItem = styled.button`
--- src/style.ts
+++ src/style.ts
@@ -1,6 +1,8 @@
 import styled, { createGlobalStyle } from 'styled-components'
 import imgDark from './images/bg-desktop-dark.jpg'
 import imgLight from './images/bg-desktop-light.jpg'
+import imgDarkCell from './images/bg-mobile-dark.jpg'
+import imgLightCell from './images/bg-mobile-light.jpg'
 
 type ThemeType = {
   tema: boolean
@@ -31,18 +33,30 @@ body{
     left:0;
     z-index:-1;
   }
+  @media(max-width:648px){
+    background-image:${(props) =>
+      props.tema == true ? `url(${imgDarkCell})` : `url(${imgLightCell})`};
+    background-size:cover;
+    background-repeat:no-repeat;
+    position:absolute;
+    top:0;
+    left:0;
+    z-index:-1;
+  }
 }
 `
 
 export const ContainerPrincipal = styled.div`
-  max-width: 640px;
+  max-width: 648px;
   width: 100%;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 100vh;
-  @media (max-width: 375px) {
-    width: 100%;
+  @media (max-width: 648px) {
+    padding: 16px;
+    align-items: center;
+    width: 100vw;
   }
 `
